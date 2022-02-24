599e72c175ad3547c59efbc44c1fed0d69add882
Toby Versteeg <toby@codegorilla.nl>
2022-02-24 14:58:36 +0100

    Factories and seeders

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name'    => 'Donald',
+            'last_name'     => 'Duck',
+            'email'         => 'donald.duck@codegorilla.nl',
+            'password'      => bcrypt('Gorilla1!'),
+            'created_at'    => now(),
+            'created_by'    => 1, 
+        ]);
+
+        DB::table('users')->insert([
+            'first_name'    => 'Katrien',
+            'last_name'     => 'Duck',
+            'email'         => 'katrien.duck@codegorilla.nl',
+            'password'      => bcrypt('Gorilla1!'),
+            'created_at'    => now(),
+            'created_by'    => 1,
+        ]);
+
+        \App\Models\User::factory(50)->create();
+    }
+}
